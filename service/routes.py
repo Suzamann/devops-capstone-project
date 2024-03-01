b3aec21d39891b8acc0a5fbc57860a16a4f5777e
Suzamann <zaman.iu21.ali1@gmail.com>
2024-03-01 02:41:25 -0500

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -61,6 +61,7 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -78,6 +79,7 @@ def list_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
+
     @app.route("/accounts/<int:account_id>", methods=["GET"])
     def get_accounts(account_id):
         """
@@ -93,6 +95,7 @@ def list_accounts():
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
+
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
@@ -112,6 +115,7 @@ def update_accounts(account_id):
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
+
 
     @app.route("/accounts/<int:account_id>", methods=["DELETE"])
     def delete_accounts(account_id):
